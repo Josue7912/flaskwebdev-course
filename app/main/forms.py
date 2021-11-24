d52282c7fa334300784f8af15c46469b184dc877
Josue7912 <81706691+Josue7912@users.noreply.github.com>
2021-11-24 20:25:59 +0100

    +last change

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Regexp
 from wtforms import widgets
 
 class IncidentForm(FlaskForm):
-    customer = StringField("Incident Number", validators=[DataRequired(), Regexp("^[0-9A-Za-z']+", 0, 'Incident case numbers must have only letters or numbers')])
+    customer = StringField("Incident Number", validators=[DataRequired(), Regexp('^HP[0-9]{6}$', flags=0, message='Please input valid Incident number')])
     dealer = SelectField("Customer Agent", default="Select your agent", choices=["","Adrian Manolias","Francesc Rebes", "Magdalena Jimenez", "Dalton Molnar"],  validators=[DataRequired()])
     rating = RadioField("Rating", choices=[('0','0'),('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10')] , widget=widgets.TableWidget(with_table_tag=False), default='5', coerce=int, validators=[DataRequired()])
     comments = TextAreaField("Comments")
--- app/main/views.py
+++ app/main/views.py
@@ -16,24 +16,26 @@ def index():
 
 @main.route('/submit', methods=['GET','POST'])
 def submit():
-    customer = request.form['customer']
-    dealer = request.form['dealer']
-    rating = request.form['rating']
-    comments = request.form['comments']
-    suggestions = request.form['suggestions']
-    if customer == '' or dealer == '':
-        return render_template('index.html', message='Please enter required fields')
-    if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
-        data = Feedback(customer, dealer, rating, comments, suggestions)
-        db.session.add(data)
-        db.session.commit()
-        msg = Message('HP Feedback', sender = 'email1@example.com', recipients = ['email2@example.com'], html=None)
-        msg.html = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li><li>Suggestions: {suggestions}</li></ul>"
-        msg.body = f"New Feedback Submission:\
-        * Customer: {customer}\
-            * Dealer: {dealer}\
-                * Rating: {rating}\
-                    * Comments: {comments}\
-                        * Suggestions: {suggestions}"
-        mail.send(msg)
-    return render_template('thankyou.html')
+    form = IncidentForm()
+    if form.validate_on_submit():
+        customer = form.customer.data
+        dealer = form.dealer.data
+        rating = form.rating.data
+        comments = form.comments.data
+        suggestions = form.suggestions.data
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments, suggestions)
+            db.session.add(data)
+            db.session.commit()
+            msg = Message('HP Feedback', sender = 'email1@example.com', recipients = ['email2@example.com'], html=None)
+            msg.html = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li><li>Suggestions: {suggestions}</li></ul>"
+            msg.body = f"New Feedback Submission:\
+            * Customer: {customer}\
+                * Dealer: {dealer}\
+                    * Rating: {rating}\
+                        * Comments: {comments}\
+                            * Suggestions: {suggestions}"
+            mail.send(msg)
+            return render_template('thankyou.html')
+        return render_template('index.html', form=form, message='You have already submmitted your feedback')
+    return render_template('index.html', form=form, message='Please input valid Incident number HPXXXXXX')
